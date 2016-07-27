d15658f8a048e88c0da4a808be8dabda6a2c479b
Samantha Raeder <samraeder08@gmail.com>
2016-07-27 16:51:10 -0500

    committing 2

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using Completed;
+
+public class Player : MovingObject {
+	public int wallDamage = 1; 
+	public int pointsperFood = 10; 
+	public int pointsperSoda = 20; 
+	public float restartLevelDelay = 1f; 
+
+	private Animator animator;
+	private int food; 
+
+	// Use this for initialization
+	protected override void Start () 
+	{
+		animator = GetComponent<Animator> (); 
+
+		food = GameManager.instance.playerFoodPoints;
+
+		base.Start ();
+	}
+
+	private void OnDisable()
+	{
+		GameManager.instance.playerFoodPoints = food;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
+protected override void AttemptMove <T> (int xDir, int yDiy)
+{
+	food--; 
+
+	base.AttemptMove <T> (xDir, yDir);
+
+	RaycastHit2D hit; 
+
+	CheckIfGameOver();
+
+	GameManager.instance.playersTurn = false; 
+}
+
+private void CheckIfGameOver()
+{
+	if (food <=0)
+		GameManager.instance.GameOver();
+}
+}
