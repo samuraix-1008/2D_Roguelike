992851c18328790df4fb3b1e42d6148358d433cc
Samantha Raeder <samraeder08@gmail.com>
2016-07-28 12:30:05 -0500

    2D rogue complete

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,15 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic; 
+using UnityEngine.UI; 
 using Completed; 
 
 public class GameManager: MonoBehaviour {
-
+	
+	public float levelStartDelay = 2f; 
+	public float turnDelay = .1f; 
 	public static GameManager instance = null; 
 	public BoardManager boardScript;
 	public int playerFoodPoints= 100; 
 	[HideInInspector] public bool playersTurn = true; 
 
-	private int level = 3; 
+	private Text levelText;
+	private GameObject levelImage; 
+	private int level = 1; 
+	private List<Enemy> enemies; 
+	private bool enemiesMoving; 
+	private bool doingSetup; 
 
 
 	void Awake ()
@@ -20,28 +29,79 @@ public class GameManager: MonoBehaviour {
 			Destroy (gameObject);
 
 		DontDestroyOnLoad (gameObject);
+		enemies = new List<Enemy> (); 
 		boardScript = GetComponent<BoardManager> (); 
 		InitGame (); 
 	}
 		
+
+	private void OnLevelWasLoaded (int index)
+	{
+		level++;
+
+		InitGame ();
+	}
 	
 		void InitGame()
 		{
+			doingSetup = true; 
+
+		levelImage = GameObject.Find ("LevelImage");
+		levelText = GameObject.Find ("LevelText").GetComponent<Text>(); 
+		levelText.text = "Day" + level;
+		levelImage.SetActive (true);
+		Invoke ("HideLevelImage", levelStartDelay);
+
+			enemies.Clear (); 
 			boardScript.SetupScene (level);
 
 	}
 
+	private void HideLevelImage()
+	{
+		levelImage.SetActive (false);
+		doingSetup = false; 
+	}
+
 	public void GameOver()
 	{
+		levelText.text = "After" + level + "days,you starved."; 
+		levelImage.SetActive (true); 
 		enabled = false;
 	}
 
+	public void AddEnemyToList(Enemy script)
+	{
+		enemies.Add (script); 
+	}
 
 
+	void Update ()
+	{
+		if (playersTurn || enemiesMoving || doingSetup)
+			return; 
 
-	void Update (){
+		StartCoroutine (MoveEnemies ());
+
 	}
    
+	IEnumerator MoveEnemies ()
+	{
+		enemiesMoving = true; 
+		yield return new WaitForSeconds (turnDelay); 
+		if (enemies.Count == 0) {
+			yield return new WaitForSeconds (turnDelay); 
+
+		}
+		for (int i = 0; i < enemies.Count; i++) {
+			enemies [i].MoveEnemy (); 
+			yield return new WaitForSeconds (enemies [i].moveTime); 
+
+		}
+		playersTurn = true; 
+		enemiesMoving = false;
 	}
+}
+
 
 
--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -5,6 +5,9 @@ public class Wall : MonoBehaviour {
 
 	public Sprite dmgSprite;
 	public int hp = 4;
+	public AudioClip chopSound1; 
+	public AudioClip chopSound2; 
+
 
 	private SpriteRenderer spriteRenderer;
 
@@ -13,7 +16,9 @@ public class Wall : MonoBehaviour {
 		spriteRenderer = GetComponent<SpriteRenderer> ();
 	}
 
-	public void DamageWall (int loss) {
+	public void DamageWall (int loss) 
+	{
+		SoundManager.instance.RandomizeSfx (chopSound1, chopSound2); 
 		spriteRenderer.sprite = dmgSprite;
 		hp -= loss;
 
